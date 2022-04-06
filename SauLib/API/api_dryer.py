7c83e02c1ec8321a2fa488086750b4906a31b1db
Luka Simić <git@kocka.tech>
2022-04-06 19:04:42 +0200

    Refactor Dryer API to use UMAK API.

--- SauLib/API/api_dryer.py
+++ SauLib/API/api_dryer.py
@@ -1,82 +1,31 @@
-from SauLib.devices.device import Device
+from SauLib.devices.device import Device, InputSource, OutputSource
 from .api_base import ApiBase
+
+TEMP_SENSOR_TEMPERATURE_BYTES = 2
 
 class TempSensor(Device):
 
     def __init__(self, port, sleep: float = 0.04):
-        self.port = port
-        self.sleep = sleep  # Time to sleep between write and read procedures
-        # Bind Sensor to Channel
-        BIND = 253  # Binding signal
-        IN = 252  # Input signal
-        CHAN_ID = 2  # Channel ID
-        INPUT_PARAM = 11  # Input Parameter
-        INPUT_SRC = 0  # Input source 0 - ADC
-        sensor_init = self.port.send(
-            [BIND, IN, CHAN_ID, INPUT_PARAM, INPUT_SRC],
-            self.sleep
-        )
-        if sensor_init[0] != 65:
-            raise Exception('Failed temperature sensor binding!')
+        Device.__init__(self, port, channel=2, sleep=sleep)
+        self.bind_input_device(InputSource.ADC, 11)
 
     def get_data(self):
-        SENSOR = 255
-        CHAN_ID = 2  # Channel ID
-        data_rsp = list(self.port.send(
-            [SENSOR, CHAN_ID],
-            self.sleep)
-        )
-        if len(data_rsp) == 3:
-            temperature = data_rsp[1] + data_rsp[2]*2**8
-        else:
-            raise Exception(
-                'Measurement Error!'
-            )
-        return temperature
-
+        return self.sensor()
 
 class Heater(Device):
 
     def __init__(self, port, sleep: float = 0.04):
-        self.port = port
-        self.sleep = sleep
-        # Bind Heater to Channel
-        BIND = 253  # Binding signal
-        OUT = 251  # Output signal
-        CHAN_ID = 2  # Channel ID
-        INPUT_SRC = 0  # Input source 0 - ADC
-        heater_init = self.port.send(
-            [BIND, OUT, CHAN_ID, INPUT_SRC],
-            self.sleep
-        )
-        if heater_init[0] != 65:
-            raise Exception(
-                'Failed actuator binding!'
-            )
+        Device.__init__(self, channel=2, port=port, sleep=sleep)
+        self.bind_output_device(OutputSource.DAC)
 
     def send_data(self, temperature: int):
-        high = temperature//256  # High Byte
-        low = temperature % 256  # Low Byte
-        ACT = 254
-        CHAN_ID = 2  # Channel ID
-
-        rsp = self.port.send(
-            [ACT, CHAN_ID, high, low],
-            self.sleep
-        )
-        if rsp[0] != 65:
-            raise Exception(
-                'Failed actuator write!'
-            )
-
+        self.actuate(temperature, TEMP_SENSOR_TEMPERATURE_BYTES)
 
 class DryerControl(Device):
 
     def __init__(self, port, sleep: float = 0.04, verbosity: bool = False):
         Device.__init__(self, channel=2, port=port, verbosity=verbosity, sleep=sleep)
-        # Init Sensor
         self.temp_sensor = TempSensor(port=self.port, sleep=self.sleep)
-        # Init Actuator
         self.heater = Heater(port=self.port, sleep=self.sleep)
 
     def send_data(self, temperature: int):
@@ -91,7 +40,6 @@ class Dryer(ApiBase):
 
     def __init__(self, port: str, verbosity: bool = False, sleep: float = 0.04):
         ApiBase.__init__(self)
-        # Dryer init
         self.dryer = DryerControl(port=port, sleep=sleep, verbosity=verbosity)
 
     def write_actuator(self, command):
